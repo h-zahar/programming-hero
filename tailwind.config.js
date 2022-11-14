7c17f4a36856fc5064eaa9f5fed88384f8b1d984
h-zahar <hz.rifat@gmail.com>
2022-11-15 02:16:35 +0600

    Setup Background

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'transparent': 'transparent',
+      // 'primary': 'linear-gradient(180deg, #18051D 18.8%, #180B34 100%)',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
   },
   plugins: [],
 }
