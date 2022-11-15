28f2f96f19181d45aa33203b294f6109b1d6c99f
h-zahar <hz.rifat@gmail.com>
2022-11-15 08:57:26 +0600

    Finished Header Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     colors: {
       'transparent': 'transparent',
       // 'primary': 'linear-gradient(180deg, #18051D 18.8%, #180B34 100%)',
+      'primary': '#F41972',
+      'secondary': '#94A3B8',
+      'classic': '#F8FAFC',
       'white': '#ffffff',
+      'dark': '#000000',
       'purple': '#3f3cbb',
       'midnight': '#121063',
       'metal': '#565584',
